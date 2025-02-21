d33ccdc2480801e424a80e936487d13e79094eb8
peehooo <suruchikumari0205@gmail.com>
2025-02-22 00:21:06 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Ensures "src" is properly resolved
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "./index.html", // Ensure Vite knows where to start
+    },
+  },
 });
